d43e881ad7d12640c7026d49db32019561b846a5
Wei <wei.kong@cocoafish.com>
2011-08-24 10:55:24 -0700

    support actionmailer change of mail settings before send

--- lib/delayed/performable_mailer.rb
+++ lib/delayed/performable_mailer.rb
@@ -3,7 +3,26 @@ require 'mail'
 module Delayed
   class PerformableMailer < PerformableMethod
     def perform
-      object.send(method_name, *args).deliver
+      #  object.send(method_name, *args).deliver
+      mail_settings = nil
+      args.each do |arg|
+        puts arg
+        if arg.kind_of? Hash
+          if arg["mail_settings"] != nil
+            # this arg provide the mail setting
+            mail_settings = arg
+            args.delete(arg)
+            break
+          end
+        end
+      end
+ 
+      msg = object.send(method_name, *args)
+      if mail_settings
+        # apply mail settings if there is one
+        msg.delivery_method.settings.merge!(mail_settings)
+      end 
+      msg.deliver
     end
   end
 
